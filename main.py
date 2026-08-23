2c65723c49271df9d1b08a6f45108288f8003548
gommeo <gommeo@educ.college-montreal.qc.ca>
2026-08-23 16:54:45 -0400

    Very scary progress. The bot is now much better and wins/draws 75-85% of the time after a large sample of games.

--- main.py
+++ main.py
@@ -1,32 +1,49 @@
 from math import e
+from enum import Enum
 import random
 
 
 class Predictor:
-    def __init__(self, num_games, learning_rate = 0.1, num_hidden_layers=1):
+    def __init__(self, num_games, learning_rate = 0.01, num_hidden_layers=1):
         self.learning_rate = learning_rate
         self.num_games = num_games
         self.layers = []
 
         for i in range(num_hidden_layers):
             if not i:
-                self.layers.append(Layer(32, num_games * 3))
+                self.layers.append(Layer(32, num_games * 6))
             else:
-                self.layers.append(Layer(32, 32))
+                self.layers.append(Layer(32, 12))
 
         self.layers.append(Layer(3, 32, last_layer=True))
 
+        self.prediction = None
+
     def forward(self, inputs):
+        self.prediction = None
+
         new_inputs = []
 
-        for game in inputs:
-            for i in range(3):
-                new_inputs.append(game[i])
+        for game_input in inputs:
+            for i in range(6):
+                new_inputs.append(game_input[i])
 
         for layer in self.layers:
             new_inputs = layer.forward(new_inputs)
 
-        return Predictor.softmax(new_inputs)
+        self.prediction = Predictor.softmax(new_inputs)
+
+        return self.prediction
+
+    def backward(self, target):
+        delta = [self.prediction[i] - target[i] for i in range(3)]
+
+        for layer in reversed(self.layers):
+            delta = layer.backward(delta)
+
+        for layer in self.layers:
+            layer.update(self.learning_rate)
+
 
     @staticmethod
     def softmax(vector):
@@ -52,7 +69,8 @@ class Layer:
             output = neuron.forward(inputs)
 
             if not self.last_layer:
-                output = max(0.0, output)
+                if output < 0:
+                    output *= 0.01
 
             neuron.output = output
 
@@ -60,14 +78,29 @@ class Layer:
 
         return outputs
 
-    def backward(self): # backpropagation research :)
-        pass
+    def backward(self, delta):
+        for i in range(len(self.neurons)):
+            if not self.last_layer and self.neurons[i].output <=0:
+                self.neurons[i].delta = delta[i] * 0.01
+            else:
+                self.neurons[i].delta = delta[i]
 
+        next_layer_deltas = [0.0 for _ in range(len(self.inputs))]
+
+        for neuron in self.neurons:
+            for j in range(len(neuron.weights)):
+                next_layer_deltas[j] += neuron.weights[j] * neuron.delta
+
+        return next_layer_deltas
+
+    def update(self, learning_rate):
+        for neuron in self.neurons:
+            neuron.update(self.inputs, learning_rate)
 
 class Neuron:
     def __init__(self, num_inputs):
         self.weights = [random.uniform(-0.2, 0.2) for _ in range(num_inputs)]
-        self.bias = 0.0
+        self.bias = 0.1
 
         self.raw_output = 0.0
         self.output = 0.0
@@ -81,21 +114,98 @@ class Neuron:
 
         return self.raw_output
 
+    def update(self, inputs, learning_rate):
+        for i in range(len(self.weights)):
+            gradient = inputs[i] * self.delta
+            self.weights[i] -= learning_rate * gradient
+
+        self.bias -= self.delta * learning_rate
+
 
 class Manager:
     def __init__(self):
-        self.num_games = 10
-        self.games = [[0, 0, 0] for _ in range(self.num_games)]
-        self.predictor = Predictor(10)
+        self.num_games = 8
+        self.games = [[0, 0, 0, 0, 0, 0] for _ in range(self.num_games)]
+        self.predictor = Predictor(self.num_games)
 
     def add_new_round(self, choice):
         del self.games[0]
         self.games.append(choice)
 
     def predict(self):
-        print(self.predictor.forward(self.games))
+        predictions = self.predictor.forward(self.games)
+
+        return random.choices([0, 1, 2], weights=predictions, k=1)[0]
+
+    def learn(self, player_choice):
+        self.predictor.backward(player_choice)
 
 
-manager = Manager()
-manager.predict()
+class Outcome(Enum):
+    PLAYER_WIN = 0
+    DRAW = 1
+    BOT_WIN = 2
 
+
+class Game:
+    def __init__(self):
+        self.manager = Manager()
+
+        self.bot_wins = 0
+        self.draws = 0
+        self.human_wins = 0
+
+        self.convert_answer = {'r': 0, 'p': 1, 's': 2}
+        self.convert_to_array = {0: [1, 0, 0], 1: [0, 1, 0], 2: [0, 0, 1]}
+
+    def run(self):
+        while True:
+            try:
+                player_choice_input = input("Rock, Paper, or Scissors\n").lower()
+
+                if player_choice_input == "r" or player_choice_input == "p" or player_choice_input == "s":
+                    prediction = self.manager.predict()
+                    player_choice = self.convert_answer.get(player_choice_input)
+                    result = Game.get_result(player_choice, prediction)
+                    self.update_stats(result)
+                    self.update_manager(player_choice, prediction)
+
+                else:
+                    print("input r, p or s")
+            except KeyboardInterrupt:
+                break
+
+    @staticmethod
+    def get_result(plyr_choice, bot_choice):
+        if plyr_choice == bot_choice:
+            return Outcome.BOT_WIN
+        elif (plyr_choice + 1) % 3 == bot_choice:
+            return Outcome.PLAYER_WIN
+        else:
+            return Outcome.DRAW
+
+    def update_stats(self, result):
+        if result == Outcome.PLAYER_WIN:
+            self.human_wins += 1
+            print("you won")
+        elif result == Outcome.BOT_WIN:
+            self.bot_wins += 1
+            print("computer won")
+        else:
+            self.draws += 1
+            print("tie")
+
+        total_games = self.bot_wins + self.draws + self.human_wins
+        print(f"Bot win%: {self.bot_wins / total_games * 100}%; draw%: {self.draws / total_games * 100}%; Human win%: {self.human_wins / total_games * 100}%; Total games: {total_games}")
+
+    def update_manager(self, plyr_choice, bot_choice):
+        plyr_choice_arr = self.convert_to_array.get(plyr_choice)
+        bot_choice_arr = self.convert_to_array.get(bot_choice)
+
+        self.manager.add_new_round(plyr_choice_arr + bot_choice_arr)
+        self.manager.learn(plyr_choice_arr)
+
+
+if __name__ == "__main__":
+    game = Game()
+    game.run()
